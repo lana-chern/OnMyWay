586388a2553867bf4f7136a8b40889ebbb36b4ba
Svetlana Chernyshkova <33090789+lana-chern@users.noreply.github.com>
2026-08-31 20:20:18 +0400

    chore: modernize project stack

--- src/main/java/com/onmyway/data/entities/District.java
+++ src/main/java/com/onmyway/data/entities/District.java
@@ -1,19 +1,21 @@
 package com.onmyway.data.entities;
 
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @Entity
 public class District {
+
     @Id
-    @GeneratedValue()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
