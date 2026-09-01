599d9d776a3cbbafd4bb3bb657c029fdfe93eebc
Svetlana Chernyshkova <33090789+lana-chern@users.noreply.github.com>
2026-09-01 13:36:14 +0400

    feat: add published place city query

--- src/main/java/com/onmyway/data/repositories/PlaceRepository.java
+++ src/main/java/com/onmyway/data/repositories/PlaceRepository.java
@@ -1,7 +1,11 @@
 package com.onmyway.data.repositories;
 
 import com.onmyway.data.entities.Place;
+import com.onmyway.data.entities.PlaceStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlaceRepository extends JpaRepository<Place, Long> {
+    List<Place> findByCityIdAndStatus(Long cityId, PlaceStatus status);
 }
