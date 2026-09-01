071f0c792d2c946f2cdefb33ba0f487b7e5fa7fb
Svetlana Chernyshkova <33090789+lana-chern@users.noreply.github.com>
2026-09-01 12:47:11 +0400

    test: cover place child entity cascade

--- src/test/java/com/onmyway/OnMyWayApplicationTests.java
+++ src/test/java/com/onmyway/OnMyWayApplicationTests.java
@@ -59,15 +59,37 @@ class OnMyWayApplicationTests {
     }
 
     @Test
+    void persistsPlaceChildrenThroughCascade() {
+        City city = city("Amsterdam", "52.367600", "4.904100");
+        Place place = place(city, "Rijksmuseum", "52.360000", "4.885200");
+
+        PlacePhoto photo = new PlacePhoto();
+        photo.setPlace(place);
+        photo.setUrl("https://example.com/photo.jpg");
+        photo.setPosition(1);
+        place.getPhotos().add(photo);
+
+        PlaceContact contact = new PlaceContact();
+        contact.setPlace(place);
+        contact.setType(PlaceContactType.WEBSITE);
+        contact.setValue("https://example.com");
+        place.getContacts().add(contact);
+
+        PlaceOpeningHours hours = openingHours(place, LocalTime.of(9, 0), LocalTime.of(18, 0));
+        place.getOpeningHours().add(hours);
+
+        placeRepository.saveAndFlush(place);
+
+        assertThat(photo.getId()).isNotNull();
+        assertThat(contact.getId()).isNotNull();
+        assertThat(hours.getId()).isNotNull();
+        assertThat(openingHoursRepository.findAll()).hasSize(1);
+    }
+
+    @Test
     void persistsMultipleOpeningIntervalsForSameDay() {
         City city = city("Test city", "52.000000", "4.000000");
-
-        Place place = new Place();
-        place.setCity(city);
-        place.setName("Test place");
-        place.setLatitude(new BigDecimal("52.000001"));
-        place.setLongitude(new BigDecimal("4.000001"));
-        place = placeRepository.saveAndFlush(place);
+        Place place = place(city, "Test place", "52.000001", "4.000001");
 
         openingHoursRepository.saveAndFlush(openingHours(place, LocalTime.of(9, 0), LocalTime.of(12, 0)));
         openingHoursRepository.saveAndFlush(openingHours(place, LocalTime.of(14, 0), LocalTime.of(18, 0)));
@@ -83,6 +105,15 @@ class OnMyWayApplicationTests {
         city.setLongitude(new BigDecimal(longitude));
         city.setTimezone("Europe/Amsterdam");
         return cityRepository.saveAndFlush(city);
+    }
+
+    private Place place(City city, String name, String latitude, String longitude) {
+        Place place = new Place();
+        place.setCity(city);
+        place.setName(name);
+        place.setLatitude(new BigDecimal(latitude));
+        place.setLongitude(new BigDecimal(longitude));
+        return placeRepository.saveAndFlush(place);
     }
 
     private PlaceOpeningHours openingHours(Place place, LocalTime opening, LocalTime closing) {
