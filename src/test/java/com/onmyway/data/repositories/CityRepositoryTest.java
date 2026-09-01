1866345c9fdf1d0d2770cceee8b2b7bd8716b4ca
Svetlana Chernyshkova <33090789+lana-chern@users.noreply.github.com>
2026-09-01 13:14:14 +0400

    fix: use Spring Boot 4 JPA test package

--- src/test/java/com/onmyway/data/repositories/CityRepositoryTest.java
+++ src/test/java/com/onmyway/data/repositories/CityRepositoryTest.java
@@ -3,7 +3,7 @@ package com.onmyway.data.repositories;
 import com.onmyway.data.entities.City;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
 import java.math.BigDecimal;
 
