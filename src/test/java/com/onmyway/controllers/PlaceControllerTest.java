faf94039a0cd8727a333cb3a6ffc1f7417368399
Svetlana Chernyshkova <33090789+lana-chern@users.noreply.github.com>
2026-09-01 14:03:13 +0400

    fix: use Spring Boot 4 MVC test package

--- src/test/java/com/onmyway/controllers/PlaceControllerTest.java
+++ src/test/java/com/onmyway/controllers/PlaceControllerTest.java
@@ -7,7 +7,7 @@ import com.onmyway.data.repositories.CityRepository;
 import com.onmyway.data.repositories.PlaceRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
