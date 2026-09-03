46f039eadcd0b766b925b7b487232787947a8ca7
Svetlana Chernyshkova <33090789+lana-chern@users.noreply.github.com>
2026-09-03 14:19:38 +0400

    feat: add user registration API

---
+++ src/main/java/com/onmyway/controllers/AuthController.java
@@ -0,0 +1,25 @@
+package com.onmyway.controllers;
+
+import com.onmyway.auth.AuthService;
+import com.onmyway.auth.api.CurrentUserResponse;
+import com.onmyway.auth.api.RegisterRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<CurrentUserResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+}
