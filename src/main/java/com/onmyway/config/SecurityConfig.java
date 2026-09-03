37765698b09a529417d0df0d565f21147802e966
Svetlana Chernyshkova <33090789+lana-chern@users.noreply.github.com>
2026-09-03 15:15:45 +0400

    feat: configure user authentication

--- src/main/java/com/onmyway/config/SecurityConfig.java
+++ src/main/java/com/onmyway/config/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.onmyway.config;
 
+import com.onmyway.data.entities.User;
+import com.onmyway.data.entities.UserStatus;
+import com.onmyway.data.repositories.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,5 +35,20 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(UserRepository userRepository) {
+        return email -> {
+            User user = userRepository.findByEmailIgnoreCase(email)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+            return org.springframework.security.core.userdetails.User
+                    .withUsername(user.getEmail())
+                    .password(user.getPasswordHash())
+                    .roles(user.getRoles().stream().map(Enum::name).toArray(String[]::new))
+                    .disabled(user.getStatus() != UserStatus.ACTIVE)
+                    .build();
+        };
     }
 }
