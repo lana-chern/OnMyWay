10739b7d6d6244d278e21cc771bd7ff6ea1166d4
Svetlana Chernyshkova <33090789+lana-chern@users.noreply.github.com>
2026-09-03 14:17:47 +0400

    feat: add user domain model

---
+++ src/main/java/com/onmyway/data/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.onmyway.data.repositories;
+
+import com.onmyway.data.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmailIgnoreCase(String email);
+    boolean existsByEmailIgnoreCase(String email);
+}
