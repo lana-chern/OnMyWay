8a3852f03137ec09c0d6c5c91dff6d45d8f0ea5e
Svetlana Chernyshkova <33090789+lana-chern@users.noreply.github.com>
2026-09-09 16:26:57 +0400

    test: configure jwt secret for application context

--- src/test/java/com/onmyway/OnMyWayApplicationTests.java
+++ src/test/java/com/onmyway/OnMyWayApplicationTests.java
@@ -2,8 +2,10 @@ package com.onmyway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "omw.jwt.secret=MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU2Nzg5MDE=")
 class OnMyWayApplicationTests {
 
     @Test
